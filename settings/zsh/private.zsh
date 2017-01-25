9bc70305af5b8ebde5511c904a438292de3009ae
AKIO <maizima44ww@gmail.com>
2017-01-26 02:07:32 +0900

    Fix zshrc

--- settings/zsh/private.zsh
+++ settings/zsh/private.zsh
@@ -1,10 +1,9 @@
 export PATH=/usr/local/bin:$PATH
-export PATH="/usr/local/sbin:$PATH"
+export PATH=/usr/local/sbin:$PATH
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export LANG=ja_JP.UTF-8
 export PATH="$HOME/.rbenv/bin:$PATH"
 
-fpath=(/usr/local/share/zsh-completions $fpath)
 setopt print_eight_bit
 source  ~/powerlevel9k/powerlevel9k.zsh-theme
 eval "$(rbenv init - zsh)"
