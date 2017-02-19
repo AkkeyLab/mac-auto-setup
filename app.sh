ea2d31a5548bfffde262c796cce2cddd3c8053e0
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-02-20 02:46:34 +0900

    Delete install slack for brew cask.

--- app.sh
+++ app.sh
@@ -31,7 +31,6 @@ brew cask install iterm2
 brew cask install onyx
 brew cask install processing
 brew cask install skype
-brew cask install slack
 brew cask install sourcetree
 brew cask install sublime-text
 brew cask install teamviewer
