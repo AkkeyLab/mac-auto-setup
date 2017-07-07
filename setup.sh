0f429f2b45a3d62c37dd3d3bcf26acc4bbe185fc
arabian9ts <nok14.tm59@gmail.com>
2017-07-07 13:26:35 +0900

    modify: the way to take latest TeX version

--- setup.sh
+++ setup.sh
@@ -111,7 +111,8 @@ echo " ------------ END ------------"
 echo " ------------ TeX ------------"
 brew cask install mactex
 # Tex Live Utility > preference > path -> /Library/TeX/texbin
-sudo /usr/local/texlive/2016/bin/x86_64-darwin/tlmgr path add
+version=$(tex -version | grep -oE '2[0-9]{3}' | head -1)
+sudo /usr/local/texlive/$version/bin/x86_64-darwin/tlmgr path add
 sudo tlmgr update --self --all
 # JPN Lang settings
 cd /usr/local/texlive/2016/texmf-dist/scripts/cjk-gs-integrate
