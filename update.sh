0d6114c052a6dc87864a47a8ed0ec40e514bbf15
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-07-16 01:04:21 +0900

    :new: Add repository update process
    `git checkout` and `git pull` command.

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cat << EOS
+
+ AkkeyLab
+
+ The elapsed time does not matter.
+ Because speed is important.
+
+EOS
+
+#
+# repository update
+#
+git checkout master
+git pull origin master
