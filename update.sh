50a0e4d12d939fa651696dda456e9ce3f1566292
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-07-16 01:06:19 +0900

    :new: Add private.zsh update process
    Set zsh setting file.

--- update.sh
+++ update.sh
@@ -13,3 +13,9 @@ EOS
 #
 git checkout master
 git pull origin master
+
+#
+# private.zsh update
+#
+cp $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/settings/zsh/private.zsh ~/.yadr/zsh/private.zsh
+source ~/.zshrc
