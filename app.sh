8d3b9e753beee75611ece2d83b97137468f70db5
AKIO <itaya_akio@matchingagent.co.jp>
2019-04-24 14:48:46 +0900

    :sparkles: Update app install list

--- app.sh
+++ app.sh
@@ -25,11 +25,8 @@ brew cask install docker
 brew cask install dropbox
 brew cask install duet
 brew cask install eclipse-java
-brew cask install firefox
-brew cask install genymotion
 brew cask install google-chrome
 brew cask install iterm2
-brew cask install processing
 brew cask install skype
 brew cask install sourcetree
 brew cask install sublime-text
@@ -37,23 +34,20 @@ brew cask install teamviewer
 brew cask install virtualbox
 brew cask install virtualbox-extension-pack
 brew cask install vlc
-brew cask install vmware-fusion
-brew cask install vmware-horizon-client
 brew cask install google-japanese-ime
 brew cask install swimat
 brew cask install spotify
 brew cask install imageoptim
-brew cask install hockey
 brew cask install soundflower
+brew cask install visual-studio-code
+brew cask install adobe-creative-cloud
+
 while true; do
   read -p 'Add "need license" apps? [Y/n]' Answer
   case $Answer in
     '' | [Yy]* )
       brew cask install microsoft-office
       brew cask install intellij-idea
-      brew cask install adobe-creative-cloud
-      brew cask install adobe-photoshop-cc
-      brew cask install adobe-illustrator-cc
       brew cask install clip-studio-paint
       break;
       ;;
@@ -67,24 +61,3 @@ while true; do
 done;
 echo " ------------ END ------------"
 
-#
-# Install web apps for optional.
-#
-echo " ---- Install option apps ----"
-while true; do
-  read -p 'Install option apps ? [Y/n]' Answer
-  case $Answer in
-    '' | [Yy]* )
-      brew cask install fritzing
-      brew cask install mysqlworkbench
-      break;
-      ;;
-    [Nn]* )
-      echo "Skip install"
-      break;
-      ;;
-    * )
-      echo Please answer YES or NO.
-  esac
-done;
-echo " ------------ END ------------"
