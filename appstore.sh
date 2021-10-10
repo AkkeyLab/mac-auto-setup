49b0251ebe266626b49b87777efc355464b5f47c
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2021-10-10 13:25:06 +0900

    :sparkles: Update app install script

--- appstore.sh
+++ appstore.sh
@@ -13,15 +13,14 @@ EOS
 #
 echo " ---- Mac App Store apps -----"
 brew install mas
-mas install 409183694  # Keynote (6.6.2)
-mas install 425424353  # The Unarchiver (3.11.1)
-mas install 803453959  # Slack (2.4.1)
-mas install 409201541  # Pages (5.6.2)
-mas install 1024640650 # CotEditor (3.1.2)
-mas install 453164367  # SystemPal (5.2)
-mas install 539883307  # LINE (4.11.1)
-mas install 412529613  # Cinch (1.2.2)
-mas install 409203825  # Numbers (3.6.2)
-mas install 476533227  # Prepo (2.2.7)
+mas install 409183694  # Keynote
+mas install 425424353  # The Unarchiver
+mas install 803453959  # Slack
+mas install 409201541  # Pages
+mas install 1024640650 # CotEditor
+mas install 453164367  # SystemPal
+mas install 539883307  # LINE
+mas install 412529613  # Cinch
+mas install 409203825  # Numbers
 mas install 549083868  # Display Menu (2.2.3)
 echo " ------------ END ------------"
