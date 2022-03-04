91f095038b825a7456fe7eae8ba3321e88611c2d
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2022-03-05 02:04:39 +0900

    :sparkles: Create arch switch script

---
+++ switch_arch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cat << EOS
+
+ AkkeyLab
+
+ The elapsed time does not matter.
+ Because speed is important.
+
+EOS
+
+read -p 'x86_64: "x", arm64: "a" and other' Answer
+case $Answer in
+  '' | [Xx]* )
+    arch -arch arm64 zsh
+    uname -m
+    ;;
+  * )
+    arch -x86_64 bash
+    zsh
+    uname -m
+    echo " ------------ END ------------"
+esac
+
