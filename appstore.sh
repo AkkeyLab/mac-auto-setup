9a6f4df20f9876379f90cca99076aa9f6bef053a
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2022-04-22 17:15:14 +0900

    :sparkles: Add AppStore App install scropt

--- appstore.sh
+++ appstore.sh
@@ -21,4 +21,7 @@ mas install 453164367  # SystemPal
 mas install 412529613  # Cinch
 mas install 409203825  # Numbers
 mas install 549083868  # Display Menu
+mas install 1549308269 # OctoLinker
+mas install 1457450145 # Octotree
+mas install 1289197285 # MindNode
 echo " ------------ END ------------"
