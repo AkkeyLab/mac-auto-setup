0862e92fc4d2ab1a950f458dc9921f0c57a0ec13
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-01-29 20:25:21 +0900

    :sparkles: Update zshrc file

--- settings/zsh/private.zsh
+++ settings/zsh/private.zsh
@@ -3,3 +3,12 @@ export LANG=en_US.UTF-8
 export SDKROOT="$(xcrun --sdk macosx --show-sdk-path)"
 
 setopt print_eight_bit
+
+# If you get a warning, do the following
+# compaudit | xargs sudo chmod g-w
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+
+  autoload -Uz compinit
+  compinit
+fi
