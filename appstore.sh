97ac59364d7b60c019cc3feaf7a5759997daba63
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2024-10-03 11:12:54 +0900

    :sparkles: Remove unused app

--- appstore.sh
+++ appstore.sh
@@ -18,10 +18,6 @@ mas install 425424353  # The Unarchiver
 mas install 409201541  # Pages
 mas install 1024640650 # CotEditor
 mas install 453164367  # SystemPal
-mas install 412529613  # Cinch
 mas install 409203825  # Numbers
-mas install 549083868  # Display Menu
-mas install 1549308269 # OctoLinker
-mas install 1457450145 # Octotree
 mas install 1289197285 # MindNode
 echo " ------------ END ------------"
