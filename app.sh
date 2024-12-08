9885dec2f524b3cf51a9c3413ecb0076cb639558
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2024-12-09 01:22:30 +0900

    :sparkles: Remove unused app

--- app.sh
+++ app.sh
@@ -12,8 +12,6 @@ EOS
 # Install web apps.
 #
 echo " ----- Install web apps ------"
-sudo softwareupdate --install-rosetta
-
 brew install --cask 1password
 brew install --cask android-studio
 brew install --cask docker
@@ -24,7 +22,6 @@ brew install --cask spotify
 brew install --cask imageoptim
 brew install --cask visual-studio-code
 brew install --cask adobe-creative-cloud
-brew install --cask gyazo
 brew install --cask zoom
 brew install --cask discord
 brew install --cask notion
@@ -33,15 +30,3 @@ brew install --cask tableplus
 brew install --cask postman
 brew install --cask clipy
 brew install --cask alfred
-
-read -p 'Do you want to install license need apps ? [y/n]' input
-case $input in
-'' | [Nn]*)
-  echo "Skip"
-  ;;
-[Yy]*)
-  brew install --cask microsoft-office
-  brew install --cask intellij-idea
-  brew install --cask clip-studio-paint
-  ;;
-esac
